60c60ca7f8efd234346aaabd0f5ec986d26e8d5f
Ilham <ilhamyahya@student.telkomuniversity.ac.id>
2023-10-17 15:47:21 +0700

    Main Menu

---
+++ Assets/Scripts/Registration.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Registration : MonoBehaviour
+{
+    [SerializeField] private TMP_InputField nameField;
+    [SerializeField] private TMP_InputField passwordField;
+    [SerializeField] private TMP_InputField passwordConfirmField;
+    [SerializeField] private Button submitButton;
+    public void CallRegister()
+    {
+        StartCoroutine(Register());
+    }
+    IEnumerator Register()
+    {
+        WWWForm form = new WWWForm();
+        form.AddField("name", nameField.text);
+        form.AddField("password", passwordField.text);
+        WWW www = new WWW("http://localhost/sqlconnect/register.php", form);
+        yield return www;
+        if (www.text == "0")
+        {
+            Debug.Log("User created successfully");
+            SceneManager.LoadScene(0);
+        }
+        else
+        {
+            Debug.LogError("User creation failed.");
+        }
+    }
+    public void VerifyInputs()
+    {
+        submitButton.interactable = (nameField.text.Length >= 4 && passwordField.text.Length >= 8 && passwordField.text == passwordConfirmField.text);
+    }
+}
